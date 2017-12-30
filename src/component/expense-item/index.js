89809bddffdd6f35cd4c6a08a22c9d8ae9449b51
jrzollin <jrzollin@yahoo.com>
2017-12-30 13:05:24 -0800

    targeting expense

--- src/component/expense-item/index.js
+++ src/component/expense-item/index.js
@@ -12,8 +12,10 @@ class ExpenseItem extends React.Component{
 
   handleDragStart(e){
     console.log('dragging');
-    let payload = e.dataTransfer.getData('text/json');
-    let card = JSON.parse(payload);
+    let card = this.props.expenses[this.props.categoryID].filter(exp => {
+      return exp.id === e.target.id;
+    })[0];
+
     console.log(card);
   }
 
@@ -31,7 +33,7 @@ class ExpenseItem extends React.Component{
     return(
       <div className='expense-item' onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
         {this.props.expenses[this.props.categoryID].map((expense,i) =>
-          <div key={expense.id} draggable="true" onDragStart={this.handleDragStart}>
+          <div key={expense.id} id={expense.id} draggable="true" onDragStart={this.handleDragStart}>
             <p> {(expense.name)} </p>
             <button onClick={() => this.props.expenseDelete(expense)}> x </button>
           </div>
